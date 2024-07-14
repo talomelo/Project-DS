5a42d160dd948d964963ccb320f29cad2f0912ef
Itallo Melo <italloml@hotmail.com>
2024-07-14 11:53:08 -0300

    projects

---
+++ Portfólio/Assets/js/script.js
@@ -0,0 +1,6 @@
+function mostryImage(img) {
+    const imageGrande = document.getElementById("imagemGrande");
+    imageGrande.src = img.src
+    imageGrande.parentElement.style.display="block"
+}
+
